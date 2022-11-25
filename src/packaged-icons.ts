7c9abdc3efed89d44a6d6a538ecdfd73dd13d5a9
Nathan Stitt <nathan@stitt.org>
2022-11-24 22:45:48 -0600

    remove ext from imports

--- src/packaged-icons.ts
+++ src/packaged-icons.ts
@@ -1,16 +1,17 @@
-import exclamationCircle from '@iconify-icons/bi/exclamation-circle-fill.js'
-import exclamationTriangle from '@iconify-icons/bi/exclamation-triangle-fill.js'
-import thumbsUp from '@iconify-icons/bi/hand-thumbs-up-fill.js'
-import xCircle from '@iconify-icons/bi/x-circle.js'
-import clock from '@iconify-icons/bi/clock.js'
+import exclamationCircle from '@iconify-icons/bi/exclamation-circle-fill'
+import exclamationTriangle from '@iconify-icons/bi/exclamation-triangle-fill'
+import thumbsUp from '@iconify-icons/bi/hand-thumbs-up-fill'
+import xCircle from '@iconify-icons/bi/x-circle'
+import clock from '@iconify-icons/bi/clock'
 import close from '@iconify-icons/bi/x-square'
-import spin from '@iconify-icons/bi/arrow-clockwise.js'
+import spin from '@iconify-icons/bi/arrow-clockwise'
 import plusSquare from '@iconify-icons/bi/plus-square'
 import minusSquare from '@iconify-icons/bi/dash-square'
 
+import type { IconifyIcon } from '@iconify/react'
 const cancel = xCircle
 
-export const ICON_DEFINITIONS = {
+export const ICON_DEFINITIONS: Record<string, IconifyIcon> = {
     thumbsUp,
     exclamationCircle,
     exclamationTriangle,
