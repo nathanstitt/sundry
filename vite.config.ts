3f1a44d312bd6d1342d8a5fd2fab0e282677a312
Nathan Stitt <nathan@stitt.org>
2023-03-12 14:00:50 -0500

    fix type outputs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import packageJson from "./package.json";
 import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
-
+import typescript from '@rollup/plugin-typescript'
 
 const packageName = packageJson.name.replace(RegExp('.*/'), '')
 
@@ -20,15 +20,18 @@ const config = defineConfig({
         lib: {
             entry: {
                 'base': './src/index.ts',
-                'form': './src/form.tsx',
-                'modal': './src/modal.tsx',
-                'menu': './src/menu.tsx',
+                'form/index': './src/form.tsx',
+                'modal/index': './src/modal.tsx',
+                'menu/index': './src/menu.tsx',
             },
         },
         rollupOptions: {
             external: ['react', '@emotion', '@emotion/css', '@emotion/styled', '@emotion/react'],
-            plugins: [excludeDependenciesFromBundle()],
+            plugins: [excludeDependenciesFromBundle(), typescript()],
             output: {
+                // entryFileNames: `[name].js`,
+                // assetFileNames: `[name].[ext]`,
+
                 // Provide global variables to use in the UMD build
                 // for externalized deps
                 globals: {
