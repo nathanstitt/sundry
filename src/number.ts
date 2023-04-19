aa16f1357fa6b49c80aee9567b74bd9a7b7d20a9
Nathan Stitt <nathan@stitt.org>
2023-04-19 15:36:46 -0500

    add ordinal

--- src/number.ts
+++ src/number.ts
@@ -2,3 +2,16 @@ export function roundToPrecision(num: number, precision = 2) {
     const pow = Math.pow(10, precision)
     return Math.round(num * pow) / pow
 }
+
+export function ordinal(n: number) {
+    if (n % 10 == 1 && n % 100 != 11) {
+        return 'st'
+    }
+    if (n % 10 == 2 && n % 100 != 12) {
+        return 'nd'
+    }
+    if (n % 10 == 3 && n % 100 != 13) {
+        return 'rd'
+    }
+    return 'th'
+}
