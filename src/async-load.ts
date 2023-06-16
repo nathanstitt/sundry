fa5639048f7bdb3b47d2d1a5f32f9b8bc2f4f296
Nathan Stitt <nathan@stitt.org>
2023-06-16 13:06:18 -0500

    import was failing on type=module

--- src/async-load.ts
+++ src/async-load.ts
@@ -2,8 +2,11 @@ import loadable, { LoadableClassComponent, DefaultComponent } from '@loadable/co
 import { retry } from './util.js'
 import { FC } from './types.js'
 
+// @loadable/component fails when imported as esm module
+const DefaultLoadable = (loadable as any)['default'] || loadable
+
 export type ImportedComponent = LoadableClassComponent<any> | FC<any>
 
 export function asyncComponentLoader<Props>(loader: () => Promise<DefaultComponent<Props>>) {
-    return loadable(() => retry(loader))
+    return DefaultLoadable(() => retry(loader))
 }
